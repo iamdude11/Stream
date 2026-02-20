2c9c5e483bc95cf53e7aa5eb04b7e4a1083f4202
iamdude11 <yedhuku2003@gmail.com>
2026-02-20 12:00:52 +0530

    Update info.py
    Jjj

--- info.py
+++ info.py
@@ -26,7 +26,7 @@ OWNER_USERNAME = environ.get("OWNER_USERNAME", 'amalvj007')
 # =========================================================
 # 🗄️ DATABASE CONNECTION
 # =========================================================
-DB_URL = environ.get('DATABASE_URI', "mongodb+srv://teshsjsg1:axxxxtz@testing.kwuyhwka.mongodb.net/?appName=testing")
+DB_URL = environ.get('DATABASE_URI', "mongodb+srv://STREAM:STREAM@cluster0.hpueciu.mongodb.net/?appName=Cluster0")
 DB_NAME = environ.get('DATABASE_NAME', "testing")
 
 # =========================================================
